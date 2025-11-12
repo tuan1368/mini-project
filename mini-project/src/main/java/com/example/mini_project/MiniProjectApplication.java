060b5a6ced5ab4f8369dc7567223e54321326498
TA <anhtuanhcmus@gmail.com>
2025-11-12 13:57:33 +0700

    Add docker file and configuration file

--- mini-project/src/main/java/com/example/mini_project/MiniProjectApplication.java
+++ mini-project/src/main/java/com/example/mini_project/MiniProjectApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MiniProjectApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MiniProjectApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MiniProjectApplication.class, args);
+    }
 
 }
+ 
